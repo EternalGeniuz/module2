1515c90ca5cdeb7685e380e1789c297882ef4779
EternalGeniuz <irak.rogov@gmail.com>
2023-12-22 01:36:08 +0300

    + main.html

--- web/views.py
+++ web/views.py
@@ -1,3 +1,11 @@
+from _datetime import datetime as dt
+
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def main_view(request):
+    year = dt.now().year
+    return render(request, "web/main.html", {
+        "year": year
+    })
