5dd0f4e8a199fc3cc9717ca278f135c7087a4457
EternalGeniuz <149954940+EternalGeniuz@users.noreply.github.com>
2024-01-22 16:30:32 +0300

    =++tables admpanel

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from web.models import TimeSlot, TimeSlotTag
+
+
+class TimeSlotAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'user', 'start_date', 'end_date', 'is_realtime', 'get_spent_time')
+    search_fields = ('title',)
+    list_filter = ('start_date', 'end_date', 'is_realtime', 'user')
+    ordering = ('-start_date',)
+    readonly_fields = ('is_realtime', 'user')
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate_spent_time()
+
+    @admin.display(description='Потрачено времени')
+    def get_spent_time(self, instance):
+        return instance.spent_time
+
+
+class TimeSlotTagAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'user')
+    search_fields = ('title',)
+    list_filter = ('user',)
+
+
+admin.site.register(TimeSlot, TimeSlotAdmin)
+admin.site.register(TimeSlotTag, TimeSlotTagAdmin)
